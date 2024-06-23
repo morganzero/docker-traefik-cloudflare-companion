fcb367ab6d282ee4dfb91d7ee93e755c487a34cf
Morgan Zero <57449785+morganzero@users.noreply.github.com>
2024-06-23 17:07:36 +0200

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Custom startup script
-
-# Log message indicating skipping of original container init script
-echo "Skipping /assets/functions/00-container due to syntax errors"
-
-# Run the specified command
-exec "$@"
